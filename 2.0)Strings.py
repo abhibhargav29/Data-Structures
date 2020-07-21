42ef8c1e7126f042ecf79ee62b9b3ac98a5b0665
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-07-21 16:48:45 +0530

    Update 2.0)Strings.py

--- 2.0)Strings.py
+++ 2.0)Strings.py
@@ -1,4 +1,5 @@
 from math import factorial
+from collections import Counter
 
 #Finds Lexicographic rank.
 #Works for strings with only upper or smaller case non repeating letters.
@@ -13,5 +14,30 @@ def LexicographicRank(s):
         rank+= mul*factorial(n-i-1)
     return rank
 
+#This function takes in a string and returns "YES" if all the elements 
+#occur same number of times or we can delete just one element to make it valid
+def isValid(s):
+    d = Counter(Counter(s).values())
+    print(d)
+    if(len(d)==1):
+        return "YES"
+    elif(len(d)>2):
+        return "NO"
+    k1,k2=d.keys()
+    v1,v2=d.values()
+    if(min(v1,v2)==1):
+        if(abs(k1-k2)==1):
+            return "YES"
+        elif(min(k1,k2)==1):
+            if(d[1]==1):
+                return "YES"
+            else:
+                return "NO"
+        else:
+            return "NO"
+    else:
+        return "NO"
+
 string = input()
 print(LexicographicRank(string))
+print(isValid(s))
