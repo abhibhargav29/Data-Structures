94b7cbfab08e09b0234a18d0343b36ab4c23074d
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-07-29 17:47:00 +0530

    Update 2.0)Strings.py

--- 2.0)Strings.py
+++ 2.0)Strings.py
@@ -174,6 +174,36 @@ def KMP(string, pat):
                 i+=1
     return " ".join(map(str, Found))
 
+#A function that takes a gene and returns the length of the smallest substring that is needed
+#to be replaced to make occurence of every letter(A,C,G,T) equal to length/4.
+#It is assumed that the length of gene passed is a multiple of 4.
+#The function is not called in driver code
+def steadyGene(gene):
+    dic = {'A':0,'T':0,'C':0,'G':0}
+    for i in gene:
+        dic[i]+=1
+    x=len(gene)
+    factor=x/4
+
+    if dic['A']==factor and dic['T']==factor and dic['C']==factor and dic['G']==factor:
+        return 0
+    
+    upper=0
+    lower=0
+    minlen=x
+    while upper<x and lower<x:
+        while (dic['A']>factor or dic['C']>factor or dic['T']>factor or\
+        dic['G']>factor) and upper<x:
+            dic[gene[upper]]-=1
+            upper+=1
+        while dic['A']<=factor and dic['C']<=factor and dic['T']<=factor\
+        and dic['G']<=factor:
+            dic[gene[lower]]+=1
+            lower+=1
+        if upper - lower < minlen :
+            minlen=upper-lower+1
+    return minlen
+
 #Driver Code
 string = input()
 print(LexicographicRank(string))
