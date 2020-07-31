cce193123578c5869ed180386edb34a4bcd9933d
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-07-31 12:24:38 +0530

    Update 4.0)LinkedList.py

--- 4.0)LinkedList.py
+++ 4.0)LinkedList.py
@@ -39,6 +39,19 @@ class SinglyLinkedList():
             self.head= New
             New.next = temp
             return
+        elif(index<0):
+            index=-index
+            prev = self.head
+            while(index>0):
+                curr=prev.next
+                index-=1
+            while(curr!=None):
+                prev = curr
+                curr = curr.next
+            temp = prev.next
+            prev.next = New
+            New.next = temp
+            return
         prev = self.head
         index-=1
         while(index>0):
@@ -47,6 +60,17 @@ class SinglyLinkedList():
         temp = prev.next
         prev.next = New
         New.next = temp
+        
+    def reverse(self):
+        prev=self.head
+        curr=self.head.next
+        prev.next=None
+        while(curr!=None):
+            next=curr.next
+            curr.next= prev
+            prev=curr
+            curr=next
+        self.head = prev
 
     def display(self):
         traverse = self.head
@@ -57,7 +81,16 @@ class SinglyLinkedList():
 #Driver Code checking each function
 LL = SinglyLinkedList()
 LL.array_to_Linklist([1,2,3,5,7], 5)
+print("Array converted to linklist: ",end="")
+LL.display()
 LL.insert(0,0)
+LL.insert(8,-1)
+print("After inertions at start and end: ",end="")
+LL.display()
 LL.delete(7)
+print("After deletion of 7: ", end="")
+LL.display()
+LL.reverse()
+print("After reversing: ",end="")
 LL.display()
             
