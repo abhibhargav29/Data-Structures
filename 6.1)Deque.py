a5e6262568232b3800a89774792409a600e74774
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-08-10 12:42:24 +0530

    Update 6.1)Deque.py

--- 6.1)Deque.py
+++ 6.1)Deque.py
@@ -1,13 +1,17 @@
 from collections import deque
 
+#Takes array and k and returns answer array.
 def SubarrMaxElement(arr, k):
     n=len(arr)
+    #This deque created will hold the necessary elements an the leftmost element will always be the maximum.
     dq = deque()
     ans=[]
+    #Iteration fo starting k elements.
     for i in range(k):
         while dq and arr[dq[-1]]<=arr[i]:
             dq.pop()
         dq.append(i)
+    #Iteration for remaining k elements.
     for i in range(k,n):
         ans.append(arr[dq[0]])
         while dq and i-dq[0]>=k:
@@ -17,8 +21,8 @@ def SubarrMaxElement(arr, k):
         dq.append(i)
     ans.append(arr[dq[0]])
     return ans
-        
+
+#Driver Code
 arr = list(map(int, input("Enter Array: ").split()))
 k = int(input("Enter k: "))
-
 print(SubarrMaxElement(arr,k))
