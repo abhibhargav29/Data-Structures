4d0fb0fb1b0ac5b21336536a616ba5434e0c9a55
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-08-13 10:44:22 +0530

    Update 7.0)Tree.py

--- 7.0)Tree.py
+++ 7.0)Tree.py
@@ -1,3 +1,4 @@
+#Tree Class
 class Tree():
     def __init__(self, data=None):
         self.val = data
@@ -24,19 +25,73 @@ class Tree():
         if(self.right!=None):
             self.right.Postorder()
         print(self.val, end=" ")
-    
+        
+    def Levelorder(self):
+        q=[]
+        if(self.val==None):
+            return
+        q.append(self)
+        #This none helps to seperate each level
+        q.append(None)
+        while(len(q)>1):
+            curr = q[0]
+            del q[0]
+            #If curr=none, we have traversed one level
+            if(curr==None):   
+                print("|",end=" ")
+                q.append(None)
+                continue
+            print(curr.val, end=" ")
+            if(curr.left!=None):
+                q.append(curr.left)
+            if(curr.right!=None):
+                q.append(curr.right)
 
+    def size(self):
+        if(self.val==None):
+            return 0
+        if(self.left!=None and self.right!=None):
+            return 1+self.left.size()+self.right.size()
+        elif(self.left!=None and self.right==None):
+            return 1+self.left.size()
+        elif(self.left==None and self.right!=None):
+            return 1+self.right.size()
+        else:
+            return 1
+
+    def k_Level(self, k):
+        if(self.val==None):
+            return
+        if(k==0):
+            print(self.val, end=" ")
+        elif(k<0):
+            return
+        else:
+            if(self.left!=None):
+                self.left.k_Level(k-1)
+            if(self.right!=None):
+                self.right.k_Level(k-1)
+
+#Driver Code
+#Tree Making
 T = Tree()
 T.val = 1
 T.left = Tree(2)
 T.right= Tree(3)
-
 T.left.left = Tree(4)
 T.left.right= Tree(5)
-
 T.right.left = Tree(6)
 T.right.right= Tree(7)
 
+#Tree size
+print("Tree size: ",T.size())
+
+#Nodes at kth level
+print("Nodes at 2nd Level: ",end="")
+T.k_Level(2)
+print()
+
+#DFS Traversals
 print("PreOrder Traversal: ", end="")
 T.Preorder()
 print()
@@ -45,3 +100,8 @@ T.Inorder()
 print()
 print("PostOrder Traversal: ", end="")
 T.Postorder()
+print()
+
+#BFS Traversal with each level seperated by "|"
+print("LevelOrder Traversal: ", end="")
+T.Levelorder()
