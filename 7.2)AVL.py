ab3c5a3e159f52b74715636dc93c0327ba0ddc1b
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-08-24 11:43:14 +0530

    Update 7.2)AVL.py

--- 7.2)AVL.py
+++ 7.2)AVL.py
@@ -8,7 +8,7 @@ class TreeNode(object):
 class AVL_Tree(object): 
 
     def insert(self, root, key): 
-        # Step 1 - Perform normal BST 
+        #Step 1 - Perform normal BST 
         if not root: 
             return TreeNode(key) 
         elif key < root.val: 
@@ -16,50 +16,46 @@ class AVL_Tree(object):
         else: 
             root.right = self.insert(root.right, key) 
   
-        # Step 2 - Update the height of the ancestor node 
+        #Step 2 - Update the height of the ancestor node 
         root.height = 1 + max(self.getHeight(root.left), 
                           self.getHeight(root.right)) 
   
-        # Step 3 - Get the balance factor 
+        #Step 3 - Get the balance factor 
         balance = self.getBalance(root) 
   
-        # Step 4 - If the node is unbalanced, then try out the 4 cases 
+        #Step 4 - If the node is unbalanced, then try out the 4 cases 
     
-        # Case 1 - Left Left 
+        #Case 1 - Left Left 
         if balance > 1 and key < root.left.val: 
             return self.rightRotate(root) 
   
-        # Case 2 - Right Right 
+        #Case 2 - Right Right 
         if balance < -1 and key > root.right.val: 
             return self.leftRotate(root) 
   
-        # Case 3 - Left Right 
+        #Case 3 - Left Right 
         if balance > 1 and key > root.left.val: 
             root.left = self.leftRotate(root.left) 
             return self.rightRotate(root) 
   
-        # Case 4 - Right Left 
+        #Case 4 - Right Left 
         if balance < -1 and key < root.right.val: 
             root.right = self.rightRotate(root.right) 
             return self.leftRotate(root) 
   
         return root 
   
-    # Recursive function to delete a node with 
-    # given key from subtree with given root. 
-    # It returns root of the modified subtree. 
+    #Recursive function to delete a node with given key from subtree with given root. 
+    #It returns root of the modified subtree. 
     def delete(self, root, key): 
   
-        # Step 1 - Perform standard BST delete 
+        #Step 1 - Perform standard BST delete 
         if not root: 
             return root 
-  
         elif key < root.val: 
             root.left = self.delete(root.left, key) 
-  
         elif key > root.val: 
             root.right = self.delete(root.right, key) 
-  
         else: 
             if root.left is None: 
                 temp = root.right 
@@ -73,24 +69,19 @@ class AVL_Tree(object):
   
             temp = self.getMinValueNode(root.right) 
             root.val = temp.val 
-            root.right = self.delete(root.right, 
-                                      temp.val) 
+            root.right = self.delete(root.right, temp.val) 
   
-        # If the tree has only one node, 
-        # simply return it 
+        # If the tree has only one node, simply return it 
         if root is None: 
             return root 
   
-        # Step 2 - Update the height of the  
-        # ancestor node 
-        root.height = 1 + max(self.getHeight(root.left), 
-                            self.getHeight(root.right)) 
+        # Step 2 - Update the height of the ancestor node 
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right)) 
   
         # Step 3 - Get the balance factor 
         balance = self.getBalance(root) 
   
-        # Step 4 - If the node is unbalanced,  
-        # then try out the 4 cases 
+        # Step 4 - If the node is unbalanced, then try out the 4 cases 
         # Case 1 - Left Left 
         if balance > 1 and self.getBalance(root.left) >= 0: 
             return self.rightRotate(root) 
