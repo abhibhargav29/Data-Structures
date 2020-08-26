e545c05c4f4eb47270da125270c35ba1fc2f8088
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-08-26 12:20:29 +0530

    Update and rename Heap.py to 8.0)Heap.py

--- 8.0)Heap.py
+++ 8.0)Heap.py
@@ -23,6 +23,7 @@ class minHeap():
     def swap(self, i, j):
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
+    #Insert while maintaining heap property.
     def insert(self, val):
         self.size+=1
         self.heap.append(val)
@@ -37,6 +38,7 @@ class minHeap():
             else:
                 break
 
+    #Display heap as parent child
     def printHeap(self):
         if(self.size==0):
             print("Empty Heap")
@@ -53,6 +55,7 @@ class minHeap():
                 break
             print()
     
+    #Helper for build heap
     def minHeapify(self, pos):
         left = self.left(pos)
         right= self.right(pos)
@@ -77,28 +80,37 @@ class minHeap():
         else:
             self.swap(smallest, pos)
 
+    #Makes heap from array
     def BuildHeap(self, arr):
         self.heap = arr
         self.size = len(arr)
         for pos in range((self.size-2)//2,-1,-1):
             self.minHeapify(pos)
 
-Heap = minHeap() 
+#Driver Code
+Heap1 = minHeap() 
 
-Heap.insert(5) 
-Heap.insert(3) 
-Heap.insert(17)
-Heap.insert(10) 
-Heap.insert(84) 
-Heap.insert(19) 
-Heap.insert(6) 
-Heap.insert(22) 
-Heap.insert(9)
+#Insertions
+Heap1.insert(5) 
+Heap1.insert(3) 
+Heap1.insert(17)
+Heap1.insert(10) 
+Heap1.insert(84) 
+Heap1.insert(19) 
+Heap1.insert(6) 
+Heap1.insert(22) 
+Heap1.insert(9)
 
-Heap.printHeap()
+#Display Heap
+print("Heap Made After Insertions: ")
+Heap1.printHeap()
 print()
 
+#Build heap from array
 arr=[5,3,17,10,84,19,6,22,9]
 Heap2 = minHeap()
 Heap2.BuildHeap(arr)
+
+#Display this Heap
+print("Heap Made From Array: ")
 Heap2.printHeap()
