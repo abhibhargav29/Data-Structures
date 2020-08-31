2067a45d5dfc2e3b0c9c2483fd6c5feb46a4eced
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-08-31 08:56:47 +0530

    Update 9.0)Graph.py

--- 9.0)Graph.py
+++ 9.0)Graph.py
@@ -1,20 +1,27 @@
-#Node
-class AdjNode():
-    def __init__(self, data):
-        self.vertex = data
-        self.next = None
-
-#Graph
 class Graph():
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = [None]*vertices
+        self.graph = [[] for j in range(vertices)]
 
-    def addEdge(self, src, dest):
-        node = AdjNode(dest)
-        node.next = self.graph[src]
-        self.graph[src] = node
+    def addEdge(self, v1, v2):
+        self.graph[v1].append(v2)
+        self.graph[v2].append(v1)
+  
+    def printGraph(self): 
+        for i in range(self.V):
+            if(len(self.graph[i])==0):
+                continue
+            print("vertex",i,"has adjacents: ",end="")
+            print(" ".join(map(str, self.graph[i])))
 
-        node = AdjNode(src) 
-        node.next = self.graph[dest] 
-        self.graph[dest] = node 
+V = 5
+graph = Graph(V)
+graph.addEdge(0, 1) 
+graph.addEdge(0, 4) 
+graph.addEdge(1, 2) 
+graph.addEdge(1, 3) 
+graph.addEdge(1, 4) 
+graph.addEdge(2, 3) 
+graph.addEdge(3, 4) 
+   
+graph.printGraph() 
